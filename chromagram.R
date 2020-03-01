7d5659ed28713cf0b8e056795f48a4fd6e303e84
Maurice <1808-maurice@hotmail.nl>
2020-03-01 13:30:52 +0100

    week 9

--- chromagram.R
+++ chromagram.R
@@ -5,12 +5,12 @@ library(compmus)
 Sys.setenv(SPOTIFY_CLIENT_ID = '7750a3cc97f3426cb5373ed0ca83a5cd')
 Sys.setenv(SPOTIFY_CLIENT_SECRET =  '31379173cced4bc4869a105ac9d92e0d')
 
-vossi <- 
+Camelot <- 
   get_tidy_audio_analysis('05JdgtCKkZ5CoOjM5KS4EG') %>% 
   select(segments) %>% unnest(segments) %>% 
   select(start, duration, pitches)
 
-vossi %>% 
+Camelot %>% 
   mutate(pitches = map(pitches, compmus_normalise, 'chebyshev')) %>% 
   compmus_gather_chroma %>% 
   ggplot(
